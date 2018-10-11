3293eeb52e1dc99b6101effb680a9695104812ba
Matt Etress <mattetress@me.com>
2018-10-11 04:30:06 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,5 +1,5 @@
-module Dance 
-  
+module Dance
+
   def twirl
     "I'm twirling"
   end
@@ -11,9 +11,9 @@ module Dance
   def pirouette
     "I'm doing a pirouette"
   end
- 
+
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-  
+
 end
